8231d408ebd3705d13d3dcc430b8ee4b0f7dd106
Vinay Salve <vinaysalve123@gmail.com>
2025-06-12 17:01:46 +0530

    chat app

---
+++ client/src/App.jsx
@@ -0,0 +1,60 @@
+import {useEffect, useState} from 'react'
+import "./App.css"
+import io from "socket.io-client";
+
+
+const socket = io.connect("http://localhost:3003");
+
+export default function App() {
+  const [message, setMessage] = useState("");
+  const [displayMessage, setDisplayMessage] = useState("");
+  // const [displayMessage, setDisplayMessage] = useState([]);
+  
+  const sendMessage = ()=>{
+    if(message.trim() === "")return;
+
+    socket.emit("sent_message/emit",{   //Transfer/Sent  message/event to the server
+      // message: message
+      message
+    })
+    // setDisplayMessage([...displayMessage, message]);
+    // setDisplayMessage(message);
+    setMessage("");
+  }
+
+  useEffect(()=>{
+    const handleReceiveMessage = (data)=>{
+      setDisplayMessage(data.message);
+      alert(data.message);
+    }
+
+    socket.on("rec_message", handleReceiveMessage);
+
+    return ()=>{
+      socket.off("rec_message", handleReceiveMessage);
+    }
+  },[])
+
+  return (
+    <>
+      <h1>Simple Chat App</h1>
+      <div className="container">
+        <input required type="text" placeholder='Type a Message' value={message} onChange={(e)=>setMessage(e.target.value)}/>
+        <button onClick={sendMessage}>Send Message</button>
+      </div>
+
+      {/* <div>
+        <ul>
+          {
+            displayMessage.filter((current, index)=> (
+              <li key={index} ><h1>{current}</h1> <br /> </li>
+              
+            ))
+          }
+        </ul>
+      </div> */}
+
+      <h1>The Message is : {displayMessage}</h1>
+    </>
+  )
+}
---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const {Server} = require("socket.io");
+const cors = require("cors");
+const PORT = 3003;
+
+const server = http.createServer(app);  //Create dummy server
+// console.log(server);
+
+app.use(cors());
+
+
+const io = new Server(server, {
+    cors:{
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+  console.log(`socket ${socket.id} connected`);
+
+  // an event was received from the client
+  socket.on("sent_message/emit",(data)=>{
+    console.log(data);
+    socket.broadcast.emit("rec_message", data);
+  })
+})
+
+server.listen(PORT, ()=>{
+    console.log("Server is running using socket.io with port:", PORT);
+});
